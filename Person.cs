3b8076246938ff1b3f66b416717e7f466c4e598a
John Holland <johnwholland@live.co.uk>
2018-10-03 17:24:01 +0100

    we doing it

---
+++ Person.cs
@@ -0,0 +1,15 @@
+namespace notbattleships
+{
+    class Person
+    {
+        string name;
+        int age;
+        string gender;
+        string hopesAndDreams;
+        
+        Person(string name, int age, string gender, string hopesAndDreams)
+        {
+            
+        }
+    }
+}
