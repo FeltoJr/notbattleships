138352c43e2a04a6f432c8fc4720eb7e4ad5bb9e
John Holland <johnwholland@live.co.uk>
2018-10-10 15:23:05 +0100

    even more i can't be stopped

--- Program.cs
+++ Program.cs
@@ -67,6 +67,7 @@ namespace notbattleships
                 {
                     Console.WriteLine("Bye!");
                     doThing = false;
+                    Console.ReadKey();
                 }
 
             }
